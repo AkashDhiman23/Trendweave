99fe4cc9d6b651e78cd5bc9f39a3d6b22c316717
AkashDhiman23 <akash.dhiman5555@gmail.com>
2024-11-20 22:43:18 +1300

    updated

--- Trendweave_users/Trendweave_users/settings.py
+++ Trendweave_users/Trendweave_users/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +35,15 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-ALLOWED_HOSTS = []
+if 'PYTHONPATH' in os.environ:
+    Debug = True
+    # Debug = False
+    ALLOWED_HOSTS = ['.ap-southeast-2.elasticbeanstalk.com']
+else:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    ALLOWED_HOSTS = []
+
 
 # AUTHENTICATION_BACKENDS = (
 #     'Userpanel.backends.EmailBackend', 
@@ -51,7 +61,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'admin_panel',  # Make sure this is included
-    'Userpanel',    # Ensure Userpanel is also included
+    'Userpanel',
+    'Trendweave_users'
     
 ]
 
@@ -101,21 +112,75 @@ WSGI_APPLICATION = 'Trendweave_users.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Trendweavedb',        
-        'USER': 'postgres',         
-        'PASSWORD': 'dhiman223', 
-        'HOST': 'localhost',            
-        'PORT': '5432',                 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'Trendweavedb',
+            'USER': 'postgres',
+            'PASSWORD': 'dhiman223',#'This is the password for your local postgres pgadmin'
+            'HOST': '', #'Localhost is empty'
+            'PORT':'', #Assumes default as 5432
+        },
+    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Trendweavedb',        
+#         'USER': 'postgres',         
+#         'PASSWORD': 'dhiman223', 
+#         'HOST': 'localhost',            
+#         'PORT': '5432',                 
+#     }
+# }
 # settings.py
-STATIC_URL = '/static/'
+
+if 'S3_BUCKET' in os.environ:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
+    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+
+    AWS_DEFAULT_ACL = None
+
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+       'CacheControl': 'max-age=86400',
+    }
+    AWS_S3_FILE_OVERWRITE = False
+    #AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
+
+    AWS_LOCATION = 'static'
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)    
+
+else:
+    STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'Userpanel/static',  
 ]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'Userpanel/static',  
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
